f81db5de7983309c2642cd95d3cea486ce74f872
Jesus Aguilar <jesusaguilar92348@gmail.com>
2024-06-17 18:35:57 -0400

    Added constructors too all objects expecpt user

--- src/Expense.java
+++ src/Expense.java
@@ -3,5 +3,37 @@ public class Expense {
 	String source;
 	double amount;
 	int yearlyfrequency; //1 for 1 time or once a year, 12 for monthly or or 24 for biweekly
-	//should add contructor(s)
+	
+	public Expense(String source, double amount, int yearlyfrequency) {
+		super();
+		this.source = source;
+		this.amount = amount;
+		this.yearlyfrequency = yearlyfrequency;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public int getYearlyfrequency() {
+		return yearlyfrequency;
+	}
+
+	public void setYearlyfrequency(int yearlyfrequency) {
+		this.yearlyfrequency = yearlyfrequency;
+	}
+	
+	
 }
--- src/Wage.java
+++ src/Wage.java
@@ -4,5 +4,35 @@ public class Wage {
 	double amount;
 	String Month;
 	
-	//should add contructor(s)
+	public Wage(String source, double amount, String month) {
+		super();
+		this.source = source;
+		this.amount = amount;
+		Month = month;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public String getMonth() {
+		return Month;
+	}
+
+	public void setMonth(String month) {
+		Month = month;
+	}
+	
 }
