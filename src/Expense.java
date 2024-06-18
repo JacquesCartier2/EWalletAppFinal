9bc3e3343e9a340726e11162981db6c257a86c8b
Jesus Aguilar-Andrade <124010062+Cetykon@users.noreply.github.com>
2024-06-18 17:12:17 -0400

    Update Expense.java ResolvedMergeConflicts

--- src/Expense.java
+++ src/Expense.java
@@ -3,33 +3,14 @@ public class Expense {
 	String source;
 	double amount;
 	int yearlyfrequency; //1 for 1 time or once a year, 12 for monthly or or 24 for biweekly
-<<<<<<< HEAD
-	//should add contructor(s)
-
-	public Expense(String source, double amount, int yearlyfrequency){
-=======
 	
 	public Expense(String source, double amount, int yearlyfrequency) {
 		super();
->>>>>>> 9ce906e195c8446cb0cf76266775f11eb1d6d836
 		this.source = source;
 		this.amount = amount;
 		this.yearlyfrequency = yearlyfrequency;
 	}
 
-<<<<<<< HEAD
-	public String getSource(){
-		return source;
-	}
-
-	public double getAmount(){
-		return amount;
-	}
-
-	public int getYearlyFrequency(){
-		return yearlyfrequency;
-	}
-=======
 	public String getSource() {
 		return source;
 	}
@@ -53,6 +34,4 @@ public class Expense {
 	public void setYearlyfrequency(int yearlyfrequency) {
 		this.yearlyfrequency = yearlyfrequency;
 	}
-	
->>>>>>> 9ce906e195c8446cb0cf76266775f11eb1d6d836
 }
