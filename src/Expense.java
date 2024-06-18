9ce906e195c8446cb0cf76266775f11eb1d6d836
Jesus Aguilar <jesusaguilar92348@gmail.com>
2024-06-17 21:00:20 -0400

    fixedConflicts

--- src/Expense.java
+++ src/Expense.java
@@ -3,33 +3,14 @@ public class Expense {
 	String source;
 	double amount;
 	int yearlyfrequency; //1 for 1 time or once a year, 12 for monthly or or 24 for biweekly
-<<<<<<< HEAD
-	//should add contructor(s)
-
-	public Expense(String source, double amount, int yearlyfrequency){
-=======
 	
 	public Expense(String source, double amount, int yearlyfrequency) {
 		super();
->>>>>>> f631f9d2614bb009343dcbb4bf077e6351f16b08
 		this.source = source;
 		this.amount = amount;
 		this.yearlyfrequency = yearlyfrequency;
 	}
 
-<<<<<<< HEAD
-	public String getSource(){
-		return source;
-	}
-
-	public double getAmount(){
-		return amount;
-	}
-
-	public int getYearlyFrequency(){
-		return yearlyfrequency;
-	}
-=======
 	public String getSource() {
 		return source;
 	}
@@ -54,6 +35,4 @@ public class Expense {
 		this.yearlyfrequency = yearlyfrequency;
 	}
 	
-	
->>>>>>> f631f9d2614bb009343dcbb4bf077e6351f16b08
 }
--- src/Wage.java
+++ src/Wage.java
@@ -4,26 +4,6 @@ public class Wage {
 	double amount;
 	String Month;
 	
-<<<<<<< HEAD
-	//should add contructor(s)
-	public Wage(String source, double amount, String Month){
-        this.source = source;
-        this.amount = amount;
-        this.Month = Month;
-    }
-
-    public String getSource(){
-        return source;
-    }
-
-    public double getAmount(){
-        return amount;
-    }
-
-    public String getMonth(){
-        return Month;
-    }
-=======
 	public Wage(String source, double amount, String month) {
 		super();
 		this.source = source;
@@ -54,6 +34,5 @@ public class Wage {
 	public void setMonth(String month) {
 		Month = month;
 	}
-	
->>>>>>> f631f9d2614bb009343dcbb4bf077e6351f16b08
+
 }
